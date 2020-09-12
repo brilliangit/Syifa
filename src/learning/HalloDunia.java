6107927604e8675547dea56f6db76404778fd839
brillian_gitlab <brillianandrienw@gmail.com>
2020-09-12 23:59:25 +0700

    halo dunia

---
+++ src/learning/HalloDunia.java
@@ -0,0 +1,10 @@
+package learning;
+
+import java.util.Scanner;
+
+public class HalloDunia {
+	public static void main(String args[])
+    {
+         System.out.println("Hallo dunia yang begitu indah");   
+    }
+}
